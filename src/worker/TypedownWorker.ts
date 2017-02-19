735f39cc22c1da3ab7a9d7cf5703f02865f806f4
Tu <hoangthtu157@gmail.com>
2017-02-19 18:47:35 +0100

    initialize service worker

---
+++ src/worker/TypedownWorker.ts
@@ -0,0 +1,17 @@
+let CACHE_NAME = "typedown-cache-v1";
+let urlsToCache = [
+    "/"
+]
+
+self.addEventListener("install", (event: InstallEvent) =>
+{
+    event.waitUntil(
+        self.caches
+            .open(CACHE_NAME)
+            .then((cache: Cache) =>
+            {
+                console.log("Opened cache");
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
